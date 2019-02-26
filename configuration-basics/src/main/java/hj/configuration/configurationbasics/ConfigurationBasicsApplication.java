d5645f6a77f8a560e2bcd54f7b07392f824cadb9
zerofunc <zerofunc@naver.com>
2019-02-26 17:37:44 +0900

    init project

---
+++ configuration-basics/src/main/java/hj/configuration/configurationbasics/ConfigurationBasicsApplication.java
@@ -0,0 +1,13 @@
+package hj.configuration.configurationbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationBasicsApplication.class, args);
+	}
+
+}
---
+++ configuration-basics/src/test/java/hj/configuration/configurationbasics/ConfigurationBasicsApplicationTests.java
@@ -0,0 +1,16 @@
+package hj.configuration.configurationbasics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigurationBasicsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
